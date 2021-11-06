58bccd706b1f0631a886f41ec2f041a5c025344a
Juan Pagola <juanpagola1992@gmail.com>
2021-11-06 14:07:20 -0300

    se agrega js del nav bar

---
+++ js/nav.js
@@ -0,0 +1,7 @@
+"use strict";
+
+let btnMenu = document.getElementById ("btn-scroll-menu");
+btnMenu.addEventListener ("click", function(){
+    document.querySelector(".nav-hidden").classList.toggle('nav-scrolled');
+} );
+    
